3f6ecca323bac30071639eb3311fe6c7f9407ca3
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2025-03-03 15:15:20 +0300

    fix: underline

--- src/shared/ui/NavList/NavList.tsx
+++ src/shared/ui/NavList/NavList.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const NavList: React.FC = () => {
+    const location = useLocation();
+
     return (
         <ul className="nav-list">
             <li className="nav-list__item">
-                <Link to="/" className="nav-list__link nav-list__link--active">Projects</Link>
+                <Link to="/" className={`nav-list__link ${location.pathname === '/' ? 'nav-list__link--active' : ''}`}>Home</Link>
             </li>
             <li className="nav-list__item">
-                <Link to="/skills" className="nav-list__link">Skills</Link>
+                <Link to="/skills" className={`nav-list__link ${location.pathname === '/skills' ? 'nav-list__link--active' : ''}`}>Skills</Link>
             </li>
             <li className="nav-list__item">
-                <Link to="/contacts" className="nav-list__link">Contacts</Link>
+                <Link to="/contacts" className={`nav-list__link ${location.pathname === '/contacts' ? 'nav-list__link--active' : ''}`}>Contacts</Link>
             </li>
             <li className="nav-list__item">
-                <Link to="/comment" className="nav-list__link">Comments</Link>
+                <Link to="/comment" className={`nav-list__link ${location.pathname === '/comment' ? 'nav-list__link--active' : ''}`}>Comments</Link>
             </li>
         </ul>
     );
 };
 
 export default NavList;
-
-
