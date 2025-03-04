40d40122a70cc9fffcdc27a220dd5b2f4707f5b1
IlyaBurlak <113059204+IlyaBurlak@users.noreply.github.com>
2025-03-04 23:10:18 +0300

    refactor: add statistic

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import {AppRoutes} from "./app/router/AppRoutes";
-import {AuthProvider} from "./context/AuthContext";
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppRoutes } from "./app/router/AppRoutes";
+import { AuthProvider } from "./context/AuthContext";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.render(
     <React.StrictMode>
         <AuthProvider>
             <Router>
+                <SpeedInsights />
                 <AppRoutes />
             </Router>
         </AuthProvider>
